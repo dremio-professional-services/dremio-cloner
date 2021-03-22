d806dcca5a2595f9aa32af88ac5e1b04e02655e9
tejkm <teju.km@outlook.com>
2021-03-22 14:21:49 +0000

    fix for the table extraction method

--- src/parse_sql.py
+++ src/parse_sql.py
@@ -1,5 +1,5 @@
 
-#######
+########
 # Copyright (C) 2019-2020 Dremio Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,52 +15,41 @@
 # limitations under the License.
 ########
 
+from moz_sql_parser import parse
+import json, re
 
-import re
+tablist = []
+def traverse(v, prefix=''):
+    if isinstance(v, dict):
+        for k, v2 in v.items():
+            p2 = "{}['{}']".format(prefix, k)
+            traverse(v2, p2)
+    elif isinstance(v, list):
+        for i, v2 in enumerate(v):
+            p2 = "{}".format(prefix)
+            traverse(v2, p2)
+    else:
+        # print('{} = {}'.format(prefix, repr(v)))
+        if prefix.endswith("['from']['value']") or prefix.endswith("['from']"):
+            # print(repr(v))
+            tablist.append(repr(v).replace("'","").replace(".","/"))
 
+    return tablist
 
-# Based on https://grisha.org/blog/2016/11/14/table-names-from-sql/
-def tables_in_query(sql_str):
+def tables_in_query(sql):
 
-	# remove the /* */ comments
-	q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
+    # remove the /* */ comments
+    sql = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql)
+    sql = re.sub("trim\(.*?\)",'trim()',sql, flags=re.DOTALL)
 
 	# remove whole line -- and # comments
-	lines = [line for line in q.splitlines() if not re.match("^\s*(--|#)", line)]
+    lines = [line for line in sql.splitlines() if not re.match("^\s*(--|#)", line)]
 
 	# remove trailing -- and # comments
-	q = " ".join([re.split("--|#", line)[0] for line in lines])
+    sql = " ".join([re.split("--|#", line)[0] for line in lines])
 
-	# split on blanks, parens and semicolons 
-	# Added ',' to support list of tables in FROM clause
-	tokens = re.split(r"[\s)(,;]+", q)
+    parsed_query = parse(sql)
+    # print(parsed_query)
+    tables = traverse(parsed_query)
 
-	# scan the tokens. if we see a FROM or JOIN, we set the get_next
-	# flag, and grab the next one (unless it's SELECT).
-
-	result = list()
-	get_next = False
-	for tok in tokens:
-		if get_next:
-			if tok.lower() not in ["", "select"]:
-				# Added support for recovering quoted names with spaces
-				if tok[0:1] == '"' and tok[-1:] != '"':
-					quoted = sql_str[sql_str.find(tok) + 1:]
-					quoted = quoted[:quoted.find('"')]
-					result.append(normalize_path(quoted))
-				else:
-					result.append(normalize_path(tok))
-			get_next = False
-		get_next = tok.lower() in ["from", "join"]
-
-	return result
-
-def normalize_path(token):
-	# [S3."asd"."ss.txt"] -> S3/asd/ss.txt
-	p = """"([^"]*)"|'([^']*)'|[\.]+"""
-	path_list = re.split(p, token)
-	path = ""
-	for item in path_list:
-		if item != None and item != '':
-			path = path + re.sub('"', '', item) + "/"
-	return path[:-1]
+    return tables
