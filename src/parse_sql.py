d505e537610f02d994057457e13cbd3ab1c21acc
tejkm <teju.km@outlook.com>
2021-04-18 22:33:40 +0100

    adding the old parser which parses partially and doesn't error out, the mozilla parser fails with some functions

--- src/parse_sql.py
+++ src/parse_sql.py
@@ -38,7 +38,54 @@ def traverse(v, prefix=''):
 
     return tablist
 
-def tables_in_query(sql):
+def tables_in_query_b(sql_str):
+
+	# remove the /* */ comments
+	q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
+	q = re.sub("trim\(.*?\)",'trim()',sql, flags=re.DOTALL|re.I)
+	q = re.sub("right\(.*?\)",'trim()',sql, flags=re.DOTALL|re.I)
+	q = re.sub("ltrim\(.*?\)",'trim()',sql, flags=re.DOTALL|re.I)
+	# remove whole line -- and # comments
+	lines = [line for line in q.splitlines() if not re.match("^\s*(--|#)", line)]
+
+	# remove trailing -- and # comments
+	q = " ".join([re.split("--|#", line)[0] for line in lines])
+
+	# split on blanks, parens and semicolons 
+	# Added ',' to support list of tables in FROM clause
+	tokens = re.split(r"[\s)(,;]+", q)
+
+	# scan the tokens. if we see a FROM or JOIN, we set the get_next
+	# flag, and grab the next one (unless it's SELECT).
+
+	result = list()
+	get_next = False
+	for tok in tokens:
+		if get_next:
+			if tok.lower() not in ["", "select"]:
+				# Added support for recovering quoted names with spaces
+				if tok[0:1] == '"' and tok[-1:] != '"':
+					quoted = sql_str[sql_str.find(tok) + 1:]
+					quoted = quoted[:quoted.find('"')]
+					result.append(normalize_path(quoted))
+				else:
+					result.append(normalize_path(tok))
+			get_next = False
+		get_next = tok.lower() in ["from", "join"]
+
+	return result
+
+def normalize_path(token):
+	# [S3."asd"."ss.txt"] -> S3/asd/ss.txt
+	p = """"([^"]*)"|'([^']*)'|[\.]+"""
+	path_list = re.split(p, token)
+	path = ""
+	for item in path_list:
+		if item != None and item != '':
+			path = path + re.sub('"', '', item) + "/"
+	return path[:-1]
+
+def tables_in_query_a(sql):
 
     # remove the /* */ comments
     sql = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql)
@@ -57,3 +104,9 @@ def tables_in_query(sql):
     tables = traverse(parsed_query)
 
     return tables
+
+def tables_in_query(sql):
+    try:
+        return tables_in_query_a(sql)
+    except:
+        return tables_in_query_b(sql)
