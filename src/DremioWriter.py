6dab4b2b32da17876dd3ca228d712d46e6184da5
Max Margalith <max.margalith@dremio.com>
2024-11-20 15:26:57 +0100

    removed v25.1 key 'parquetDataFormat.ignoreOtherFileFormats' from _promote_pds() for backwards compatibility with v25.0

--- src/DremioWriter.py
+++ src/DremioWriter.py
@@ -673,6 +673,12 @@ class DremioWriter:
 		if 'format' in entity:
 			if 'ignoreOtherFileFormats' in entity['format']:
 				entity['format'].pop("ignoreOtherFileFormats")
+			if 'parquetDataFormat' in entity['format']:
+				if 'ignoreOtherFileFormats' in entity['format']['parquetDataFormat']:
+					entity['format']['parquetDataFormat'].pop("ignoreOtherFileFormats")
+		if 'accelerationRefreshPolicy' in entity:
+			if 'sourceRefreshOnDataChanges' in entity['accelerationRefreshPolicy']:
+				entity['accelerationRefreshPolicy'].pop("sourceRefreshOnDataChanges")
 		# Process ACL as needed
 		if not self._process_acl(entity, ignore_missing_acl_user_flag, ignore_missing_acl_group_flag):
 			# Skip this entity due to ACL processing errors
